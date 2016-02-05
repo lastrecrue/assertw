df093340fa0b5f1a8cd5e2e2cccca0831671f67f
achraf lahmourate <achraf.lahmourate@gmail.com>
2016-02-05 11:42:24 +0100

    clean format

--- src/main/java/com/orange/assertw/core/api/WaitAssertions.java
+++ src/main/java/com/orange/assertw/core/api/WaitAssertions.java
@@ -1,9 +1,18 @@
 package com.orange.assertw.core.api;
 
+/**
+ * .
+ * 
+ * @author Achraf LAHMOURATE
+ */
 public class WaitAssertions {
-
+	/**
+	 * 
+	 * @param actual
+	 * @return
+	 */
 	public static AbstractWaitBooleanAssert<?> assertThat(Boolean actual) {
-		 return AssertionsForWaitClassTypes.assertThat(actual);
+		return AssertionsForWaitClassTypes.assertThat(actual);
 	}
 
 }
--- src/test/java/com/orange/assertw/core/api/WaitAssertTest.java
+++ src/test/java/com/orange/assertw/core/api/WaitAssertTest.java
@@ -4,22 +4,25 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import com.jayway.awaitility.core.ConditionTimeoutException;
-import com.orange.assertw.core.api.WaitAssertions;
 
+/**
+ * .
+ * 
+ * @author Achraf LAHMOURATE
+ */
 public class WaitAssertTest {
 
-	@Test
-	public void testBooleanAtMostModification() {
-		try {
-			WaitAssertions.assertThat(true).atMost(2).isEqualTo(false);
-		} catch (ConditionTimeoutException cte) {
-			Assertions.assertThat(cte).hasMessageContaining("2 seconds");
-		}
-	}
-	
-	
-	@Test
-	public void testBooleanTrue() {
-			WaitAssertions.assertThat(true).atMost(2).isEqualTo(true);
-	}
+  @Test
+  public void testBooleanAtMostModification() {
+    try {
+      WaitAssertions.assertThat(true).atMost(2).isEqualTo(false);
+    } catch (ConditionTimeoutException cte) {
+      Assertions.assertThat(cte).hasMessageContaining("2 seconds");
+    }
+  }
+
+  @Test
+  public void testBooleanTrue() {
+    WaitAssertions.assertThat(true).atMost(2).isEqualTo(true);
+  }
 }
